2fce7cc273724b528a96129a73ed231dd27fce75
Odjoutchone Sylvestre Tokou <odjoutchone@yahoo.fr>
2023-04-24 14:54:51 -0400

    Adds index.js

--- index.js
+++ index.js
@@ -10,7 +10,22 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
-const titleCased = () => {
-  return tutorials
-}
+function titleCased() {
+    const tutorials = [
+      "what does the this keyword mean?",
+      "What Is The Constructor OO Pattern?",
+      "Implementing Blockchain Web API",
+      "the test driven development workflow",
+      "what is NaN and how can we check for it",
+      "what is the difference between stopPropagation and preventDefault?",
+      "immutable state and pure functions",
+      "what is the difference between == and ===?",
+      "what is the difference between event capturing and bubbling?",
+      "what is JSONP?"
+    ];
+    return tutorials.map(function(tutorial) {
+      return tutorial.split(' ').map(function(word) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      }).join(' ');
+    })
+  }
